824407e745aa7b1892126e55178659f48d5afa85
Juan Sebastian Gaviria Prada <jgaviria@supervisa.co>
2023-12-04 23:05:08 -0500

    :construction: Implementación de libreria nativa de node para conexión con PostgreSQL

--- nestjs-modular/src/app.module.ts
+++ nestjs-modular/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
+import { Client } from 'pg';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,6 +9,20 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
 
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect();
+client.query('select * from tasks', (err, res) => {
+  console.error(err);
+  console.log(res.rows);
+});
 
 @Module({
   imports: [ConfigModule.forRoot({
